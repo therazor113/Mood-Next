760a64f22859948f8ff6c63b59f3063a99210ae0
Razor <therazor113@gmail.com>
2022-08-02 12:02:08 -0400

    Created header component

---
+++ src/components/core/HeaderComponent/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const HeaderComponent = () => {
+  return (
+    <Head>
+      <title>Mood-next</title>
+      <meta name="description" content="Created with nextjs" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
+
+export default HeaderComponent
