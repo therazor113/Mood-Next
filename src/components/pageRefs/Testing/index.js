a866257599bec812e89b594bc17afc0ce84bec7e
Razor <therazor113@gmail.com>
2022-08-11 13:57:14 -0400

    Created temp user creator

--- src/components/pageRefs/Testing/index.js
+++ src/components/pageRefs/Testing/index.js
@@ -1,6 +1,44 @@
+import { useState } from 'react'
+
 const Testing = () => {
+  const [inputValue, setInputValue] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleEnter = async () => {
+    if (!inputValue) return
+    try {
+      const createRes = await fetch('/api/UsersApi/CreateUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: inputValue })
+      })
+      const data = await createRes.json()
+      setMessage(data)
+      setTimeout(() => setMessage(''), 1500)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+
+  const handleChange = (e) => {
+    if (!/^[a-zA-Z]*$/g.test(e.target.value) || e.target.value.length > 10) return
+    setInputValue(e.target.value)
+  }
+
+  const handleKeyUp = (e) => {
+    if (e.key === 'Enter') {
+      handleEnter()
+    }
+  }
+
   return (
-    <button onClick={() => console.log('testing')}>Testing</button>
+    <main>
+      <button onClick={handleEnter}>Testing</button>
+      <input value={inputValue} onChange={handleChange} onKeyUp={handleKeyUp} autoFocus/>
+      <p>{message}</p>
+    </main>
   )
 }
 
