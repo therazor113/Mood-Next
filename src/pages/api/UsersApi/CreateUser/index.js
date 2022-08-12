d1ebe357f71c3d916a06a7acb5096bbde1733349
Razor <therazor113@gmail.com>
2022-08-12 12:28:38 -0400

    Created fail case for other type of requests

--- src/pages/api/UsersApi/CreateUser/index.js
+++ src/pages/api/UsersApi/CreateUser/index.js
@@ -2,14 +2,18 @@
 import pool from 'lib/db'
 
 const CreateUserByName = async (req, res) => {
-  const { name } = req.body
   try {
-    const user = await pool.query('SELECT * FROM users WHERE name = $1', [name])
-    if (user.rows[0]) {
-      res.json('Please use a different name')
+    if (req.method === 'POST') {
+      const { name } = req.body
+      const user = await pool.query('SELECT * FROM users WHERE name = $1', [name])
+      if (user.rows[0]) {
+        res.json('Please use a different name')
+      } else {
+        const newUser = await pool.query('INSERT INTO users (name) VALUES ($1) RETURNING *', [name])
+        res.json(`User ${name} created`)
+      }
     } else {
-      const newUser = await pool.query('INSERT INTO users (name) VALUES ($1) RETURNING *', [name])
-      res.json(`User "${name}" created`)
+      res.json('only \'POST\' request here')
     }
   } catch (err) {
     console.error(err.message)
