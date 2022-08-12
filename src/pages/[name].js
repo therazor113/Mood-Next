831796f082b1c1ec77b1bdd3748096846d673006
Razor <therazor113@gmail.com>
2022-08-12 12:26:06 -0400

    Added number fetching for moods

--- src/pages/[name].js
+++ src/pages/[name].js
@@ -1,16 +1,5 @@
 import MoodChart from 'components/pageRefs/MoodChart'
-
-const UserInfo = ({ user, stats }) => {
-  return (
-    <div>
-      <h2>{user.name}</h2>
-      <h2>{user.userid}</h2>
-      <MoodChart stats={stats} />
-    </div>
-  )
-}
-
-export default UserInfo
+import CreateEntry from 'components/utilities/Testing/CreateEntry'
 
 export const getStaticPaths = async () => {
   const res = await fetch('http://localhost:3000/api/UsersApi/UsersData')
@@ -32,9 +21,26 @@ export const getStaticProps = async (context) => {
   const userRes = await fetch(`http://localhost:3000/api/UsersApi/UsersData/${name}`)
   const userData = await userRes.json()
   const statsRes = await fetch(`http://localhost:3000/api/UserDataApi/MoodData/${userData.userid}`)
+  const statNums = []
   const statsData = await statsRes.json()
+  for (let i = 0; i < statsData.length; i++) {
+    statNums.push(statsData[i].number)
+  }
 
   return {
-    props: { user: userData, stats: statsData }
+    props: { user: userData, stats: statsData, statNumbers: statNums }
   }
 }
+
+const UserInfo = ({ user, stats, statNumbers }) => {
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <h2>{user.userid}</h2>
+      <MoodChart stats={stats} statNumbers={statNumbers} />
+      <CreateEntry userid={user.userid} />
+    </div>
+  )
+}
+
+export default UserInfo
