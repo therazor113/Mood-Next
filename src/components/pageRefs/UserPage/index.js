a73235a654f22e9b16ff7e8ad9591d03a855c456
Razor <therazor113@gmail.com>
2022-08-11 20:29:58 -0400

    Added temp userPage

---
+++ src/components/pageRefs/UserPage/index.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+
+const UserPage = () => {
+  useEffect(() => {
+    handleUser()
+  })
+
+  const handleUser = () => {
+  }
+
+  return (
+    <div>
+      <h2>Welcome!</h2>
+    </div>
+  )
+}
+
+export default UserPage
