1411cb614d580a83a11856ddfc3a60f9bd733ec5
Razor <therazor113@gmail.com>
2022-08-13 23:48:11 -0400

    Added EXISTS queries to stack

--- src/pages/api/UserDataApi/MoodData/CreateJournal/[userid]/index.js
+++ src/pages/api/UserDataApi/MoodData/CreateJournal/[userid]/index.js
@@ -4,17 +4,28 @@ import pool from 'lib/db'
 const CreateJournalEntry = async (req, res) => {
   try {
     if (req.method === 'POST') {
-      const { entry } = req.body
-      const { mood } = req.body
-      // const user = await pool.query('SELECT * FROM users WHERE date = $1', [entryDate])
-      // if (user.rows[0]) {
-      // res.json('Entry already made for that date')
-      // } else {
-      const journalEntry = await pool.query('INSERT INTO dailymoods (mood, journal, userid) VALUES ($1, $2, $3) RETURNING *',
-        [mood, entry, req.query.userid]
+      const { entry, mood, date, fullDate } = req.body
+      const entryListFull = await pool.query(
+        `SELECT * FROM dailymoods WHERE userid = $1 AND CAST(date AS TEXT) LIKE '${date}%'`,
+        [req.query.userid]
+      )
+      if (entryListFull.rows.length >= 8) {
+        res.json('You\'ve finished your entries for today!')
+        return
+      }
+      const entryExists = await pool.query(
+        'SELECT EXISTS (SELECT * FROM dailymoods WHERE userid = $1 AND date = $2)',
+        [req.query.userid, fullDate]
+      )
+      if (entryExists.rows[0].exists) {
+        res.json('Please wait 1 hr for next entry')
+        return
+      }
+      await pool.query(
+        'INSERT INTO dailymoods (mood, journal, date, userid) VALUES ($1, $2, $3, $4)',
+        [mood, entry, fullDate, req.query.userid]
       )
       res.json(`Journal: ${entry} with mood: ${mood} created`)
-      // }
     } else {
       res.json('only \'POST\' request here')
     }
