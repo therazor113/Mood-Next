b029fcd9ec1abbfc239e06d4c7e967697bd86c70
Razor <therazor113@gmail.com>
2022-08-13 23:47:36 -0400

    Added LIMIT to query

--- src/pages/api/UserDataApi/MoodData/[userid]/index.js
+++ src/pages/api/UserDataApi/MoodData/[userid]/index.js
@@ -2,8 +2,10 @@ import pool from 'lib/db'
 
 const getAllDailyMoodsById = async (req, res) => {
   try {
-    const dailyMoods = await pool.query('SELECT id, mood, journal FROM dailymoods WHERE userid = $1 ORDER BY id',
-      [req.query.userid])
+    const dailyMoods = await pool.query(
+      'SELECT id, mood, journal FROM dailymoods WHERE userid = $1 ORDER BY date LIMIT 8',
+      [req.query.userid]
+    )
     res.json(dailyMoods.rows)
   } catch (err) {
     console.error(err.message)
