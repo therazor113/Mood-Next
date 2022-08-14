4ba58698313d634a79f76681463309791caa38e3
Razor <therazor113@gmail.com>
2022-08-13 23:49:59 -0400

    Added EXISTS to check if user already exists

--- src/pages/api/UsersApi/CreateUser/index.js
+++ src/pages/api/UsersApi/CreateUser/index.js
@@ -5,15 +5,19 @@ const CreateUserByName = async (req, res) => {
   try {
     if (req.method === 'POST') {
       const { name } = req.body
-      const user = await pool.query('SELECT * FROM users WHERE name = $1', [name])
-      if (user.rows[0]) {
+      if (!name) {
+        res.json('Please enter a name')
+        return
+      }
+      const userExists = await pool.query('SELECT EXISTS (SELECT name FROM users WHERE name = $1)', [name])
+      if (userExists.rows[0].exists) {
         res.json('Please use a different name')
       } else {
         const newUser = await pool.query('INSERT INTO users (name) VALUES ($1) RETURNING *', [name])
-        res.json(`User ${name} created`)
+        res.json(newUser.rows[0])
       }
     } else {
-      res.json('only \'POST\' request here')
+      res.json('only \'POST\' requests here')
     }
   } catch (err) {
     console.error(err.message)
