4484dc5d287af86eaac463dd05c0766bd58db7b8
Razor <therazor113@gmail.com>
2022-08-14 10:59:37 -0400

    Replaced outdated titles of components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import Layout from 'components/core/Layout'
-import CreateUser from 'components/utilities/Testing/CreateUser'
-import LoginUser from 'components/utilities/Testing/LoginUser'
+import CreateUser from 'components/utilities/CreateUserForm'
+import SignInForm from 'components/utilities/SignInForm'
 
 function Home () {
   return (
     <Layout>
       <div style={{ display: 'flex', gap: '2rem' }}>
         <CreateUser />
-        <LoginUser />
+        <SignInForm />
       </div>
     </Layout>
   )
