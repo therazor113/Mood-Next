4de6f81540753cc88586a864011a13e523cf3d99
Razor <therazor113@gmail.com>
2022-08-13 23:46:50 -0400

    Imported journal cards & Created stats update func

--- src/pages/[name].js
+++ src/pages/[name].js
@@ -1,6 +1,7 @@
+import { useState } from 'react'
 import MoodChart from 'components/pageRefs/MoodChart'
 import CreateEntry from 'components/utilities/Testing/CreateEntry'
-import UpdateEntry from 'components/utilities/Testing/UpdateEntry'
+import JournalList from 'components/pageRefs/JournalList'
 
 export const getStaticPaths = async () => {
   const res = await fetch('http://localhost:3000/api/UsersApi/UsersData')
@@ -22,31 +23,38 @@ export const getStaticProps = async (context) => {
   const userRes = await fetch(`http://localhost:3000/api/UsersApi/UsersData/${name}`)
   const userData = await userRes.json()
   const statsRes = await fetch(`http://localhost:3000/api/UserDataApi/MoodData/${userData.userid}`)
-  const statNums = []
+  const statMoods = []
   const statsData = await statsRes.json()
   for (let i = 0; i < statsData.length; i++) {
-    statNums.push(statsData[i].mood)
+    statMoods.push(statsData[i].mood)
   }
 
   return {
-    props: { user: userData, stats: statsData, statMoods: statNums }
+    props: { statsData, statMoods, user: userData }
   }
 }
 
-const UserInfo = ({ user, stats, statMoods }) => {
+const UserInfo = ({ statsData, statMoods, user }) => {
+  const [statMoodsArr, setStatMoodsArr] = useState(statMoods)
+  const [statsArr, setStatsArr] = useState(statsData)
+
+  const updateStats = async () => {
+    const statsRes = await fetch(`http://localhost:3000/api/UserDataApi/MoodData/${user.userid}`)
+    const statMoods = []
+    const statsData = await statsRes.json()
+    for (let i = 0; i < statsData.length; i++) {
+      statMoods.push(statsData[i].mood)
+    }
+    setStatMoodsArr(statMoods)
+    setStatsArr(statsData)
+  }
   return (
     <div>
       <h2>{user.name}</h2>
       <h2>{user.userid}</h2>
-      <MoodChart stats={stats} statMoods={statMoods} />
-      <CreateEntry userid={user.userid} />
-      {stats.map((userStats) => (
-          <ul style={{ listStyleType: 'none' }} key={userStats.id}>
-            <li>Mood: {userStats.mood}</li>
-            <li>Journal Entry: {userStats.journal}</li>
-            <UpdateEntry journalId={userStats.id} />
-          </ul>
-      ))}
+      <MoodChart stats={statsArr} statMoods={statMoodsArr} />
+      <CreateEntry updateStats={updateStats} userid={user.userid} />
+      <JournalList statsArr={statsArr} updateStats={updateStats} />
     </div>
   )
 }
