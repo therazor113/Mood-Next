9b4fed8a244a20909dbc41ab697ea7a551fc13ea
Razor <therazor113@gmail.com>
2022-08-14 10:57:31 -0400

    Edit to border color & text alignment

--- src/components/core/FooterComponent/index.js
+++ src/components/core/FooterComponent/index.js
@@ -5,12 +5,12 @@ const FooterComponent = () => {
   return (
     <main>
         <footer className={classes.footer}>
+          Powered by{' '}
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-          Powered by{' '}
           <span className={classes.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
