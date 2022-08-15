84ae219f54dfb0b7fd8ac2e2a592d9b3b6b69423
Razor <therazor113@gmail.com>
2022-08-15 00:23:38 -0400

    Made chat more modular & added components

--- src/components/pageRefs/MoodChart/index.js
+++ src/components/pageRefs/MoodChart/index.js
@@ -1,8 +1,13 @@
-import { Chart } from 'react-chartjs-2'
+import { Chart, getElementAtEvent } from 'react-chartjs-2'
+import { useRef, useState } from 'react'
+import SingleEntry from '../SingleEntry'
 
 import classes from './styles.module.scss'
+import EntriesList from '../EntriesList'
 
-const MoodChart = ({ stats, statMoods }) => {
+const MoodChart = ({ title, stats, Moods, updateStats, updateWeekStats }) => {
+  const [cardIndex, setCardIndex] = useState(null)
+  const chartRef = useRef()
   const options = {
     responsive: true,
     scales: {
@@ -12,6 +17,18 @@ const MoodChart = ({ stats, statMoods }) => {
         stepSize: 1,
         ticks: {
           precision: 0
+        }
+      },
+      x: {
+        ticks: {
+          callback: function (item, index) {
+            if (!stats[0]?.journal) {
+              const list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+              return list[index]
+            } else {
+              return this.getLabelForValue(item)
+            }
+          }
         }
       }
     },
@@ -29,12 +46,20 @@ const MoodChart = ({ stats, statMoods }) => {
       },
       title: {
         display: true,
-        text: 'Today\'s Moods'
+        text: title
       },
       tooltip: {
         callbacks: {
           label: (item) => {
-            return `Entry: ${item.dataset.entry[item.dataIndex].journal}`
+            if (!item.dataset.entry[item.dataIndex].journal) {
+              const entryList = []
+              for (let i = 0; i < item.dataset.entry[item.dataIndex][1].length; i++) {
+                entryList.push(i + 1 + ': ' + item.dataset.entry[item.dataIndex][1][i].journal)
+              }
+              return `Entries: ${entryList.join('\r\n')}`
+            } else {
+              return `Entry: ${item.dataset.entry[item.dataIndex].journal}`
+            }
           },
           labelColor: (item) => {
             return {
@@ -49,7 +74,7 @@ const MoodChart = ({ stats, statMoods }) => {
       }
     }
   }
-  const labels = ['8:00 AM', '10:00 AM', '12:00 AM', '2:00 PM', '4:00 PM', '6:00 PM', '8:00 PM', '10:00 PM']
+  const labels = [1, 2, 3, 4, 5, 6, 7, 8]
   const colors = ['red', 'pink', 'orange', 'blue', 'lightblue', 'purple', 'teal', 'lightgreen', 'green']
 
   const data = {
@@ -57,13 +82,35 @@ const MoodChart = ({ stats, statMoods }) => {
     datasets: [{
       colors,
       entry: stats,
-      data: statMoods
+      data: Moods
     }]
   }
+
+  const handleClick = (e) => {
+    const item = getElementAtEvent(chartRef.current, e)
+    if (!item.length) return
+    if (cardIndex === item[0].index) {
+      setCardIndex(null)
+    } else {
+      setCardIndex(item[0].index)
+    }
+  }
+
   return (
-    <div className={classes.container}>
-      <Chart type='line' options={options} data={data} />
-    </div>
+    <>
+      <div className={classes.container}>
+        <Chart type='line' options={options} data={data} onClick={handleClick} ref={chartRef} />
+      </div>
+      {stats[0]?.journal
+        ? <SingleEntry statEntry={stats[cardIndex]} updateStats={updateStats} />
+        : <EntriesList
+          statsArr={stats}
+          updateStats={updateStats}
+          cardIndex={cardIndex}
+          updateWeekStats={updateWeekStats}
+        />
+      }
+    </>
   )
 }
 
