0bedd9f1240e178297819b5661dea31092c2ecb5
Razor <therazor113@gmail.com>
2022-08-15 00:28:41 -0400

    Moved into dynamic folder

---
+++ src/pages/api/UserDataApi/GetEntries/[userid]/[date]/index.js
@@ -0,0 +1,15 @@
+import pool from 'lib/db'
+
+const getAllMoodsById = async (req, res) => {
+  try {
+    const Moods = await pool.query(
+      'SELECT id, mood, journal, date, time, weekday FROM moods WHERE userid = $1 AND date = $2 ORDER BY time LIMIT 8',
+      [req.query.userid, req.query.date]
+    )
+    res.json(Moods.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+}
+
+export default getAllMoodsById
