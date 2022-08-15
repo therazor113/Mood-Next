80e2829cd7b22685707415d4fd637e3f1dfeedec
Razor <therazor113@gmail.com>
2022-08-15 00:29:30 -0400

    Updated syntax for db

--- src/pages/api/UserDataApi/UpdateEntry/[id]/index.js
+++ src/pages/api/UserDataApi/UpdateEntry/[id]/index.js
@@ -6,7 +6,7 @@ const UpdateEntryByid = async (req, res) => {
     if (req.method === 'PUT') {
       const { entry } = req.body
       const { mood } = req.body
-      await pool.query('UPDATE dailymoods SET mood = $1, journal = $2 WHERE id = $3',
+      await pool.query('UPDATE moods SET mood = $1, journal = $2 WHERE id = $3',
         [mood, entry, req.query.id]
       )
       res.json(`Journal Updated: entry ${entry} mood: ${mood}`)
