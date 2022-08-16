335b3f5d3e9fca6163bb0144e077984c8d94d756
Razor <therazor113@gmail.com>
2022-08-15 20:51:44 -0400

    Fixed syntax

--- src/pages/api/UserDataApi/GetEntries/[userid]/[date]/index.js
+++ src/pages/api/UserDataApi/GetEntries/[userid]/[date]/index.js
@@ -2,11 +2,11 @@ import pool from 'lib/db'
 
 const getAllMoodsById = async (req, res) => {
   try {
-    const Moods = await pool.query(
+    const moods = await pool.query(
       'SELECT id, mood, journal, date, time, weekday FROM moods WHERE userid = $1 AND date = $2 ORDER BY time LIMIT 8',
       [req.query.userid, req.query.date]
     )
-    res.json(Moods.rows)
+    res.json(moods.rows)
   } catch (err) {
     console.error(err.message)
   }
