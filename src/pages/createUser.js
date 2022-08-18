70fc980a6d0b333bc79db60a7cde7c1eae4a0a34
Razor <therazor113@gmail.com>
2022-08-17 21:24:59 -0400

    Added CreateUser page

---
+++ src/pages/createUser.js
@@ -0,0 +1,12 @@
+import Layout from 'components/core/Layout'
+import CreateCard from 'components/pageRefs/CreateCard'
+
+function Home () {
+  return (
+    <Layout>
+      <CreateCard />
+    </Layout>
+  )
+}
+
+export default Home
