5103d32b2a511379589021160df9f312454239b8
Razor <therazor113@gmail.com>
2022-08-18 18:17:15 -0400

    Added favicon context

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
+import { useContext, useEffect } from 'react'
+import FaviconContext from 'contexts/FaviconContext'
 import Layout from 'components/core/Layout'
 import LoginCard from 'components/pageRefs/LoginCard'
 
 function Home () {
+  const { setFavicon } = useContext(FaviconContext)
+  useEffect(() => {
+    setFavicon('/login.ico')
+  }, [setFavicon])
   return (
-    <Layout>
+    <Layout title={'Login'}>
       <LoginCard />
     </Layout>
   )
