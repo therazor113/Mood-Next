17572ca9d51a7d413b8fee8849b66373ff771adb
Razor <therazor113@gmail.com>
2022-08-17 21:24:03 -0400

    Merged with files in pageRefs

--- src/components/utilities/CreateUserForm/CreateUser/index.js
+++
@@ -1,7 +0,0 @@
-const CreateUser = async (handleFetch, inputValue) => {
-  const path = '/UsersApi/CreateUser'
-  const body = { name: inputValue }
-  return await handleFetch(path, 'POST', body)
-}
-
-export default CreateUser
--- src/components/utilities/SignInForm/UserSignIn/index.js
+++
@@ -1,6 +0,0 @@
-const UserSignIn = async (handleFetch, inputValue) => {
-  const path = `/UsersApi/GetUsers/${inputValue}`
-  return await handleFetch(path, 'GET')
-}
-
-export default UserSignIn
