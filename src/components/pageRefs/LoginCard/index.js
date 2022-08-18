6d1802a4a3f2df2946298d8560c5acecfb302e83
Razor <therazor113@gmail.com>
2022-08-18 18:22:48 -0400

    Updated Link href

--- src/components/pageRefs/LoginCard/index.js
+++ src/components/pageRefs/LoginCard/index.js
@@ -5,19 +5,19 @@ import classes from './styles.module.scss'
 
 const LoginCard = () => {
   return (
-      <div className={classes.container}>
-        <div className={classes.header}>
-          <h1>User Login</h1>
-          <h2>Welcome! Enter your name to sign in<br/>and view your charts!</h2>
-        </div>
-        <SignInForm />
-        <div className={classes.footer}>
-          <p>Don&apos;t have an account?</p>
-          <Link href='/createUser'>
-            <span className={classes.link}>Create Account</span>
-          </Link>
-        </div>
+    <div className={classes.container}>
+      <div className={classes.header}>
+        <h1>User Login</h1>
+        <h2>Welcome! Enter your name to sign in<br/>and view your charts!</h2>
       </div>
+      <SignInForm />
+      <div className={classes.footer}>
+        <p>Don&apos;t have an account?</p>
+        <Link href='/createLogin'>
+          <span className={classes.link}>Create Account</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
