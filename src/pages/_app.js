53c670f3b565e559a239ef7bc02ca4abe169f0a9
Razor <therazor113@gmail.com>
2022-08-19 16:58:30 -0400

    Added font fix for chart icons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,11 @@ import 'lib/prototypes/getWeek'
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
+import { defaults } from 'chart.js'
+// Disabled autoAddCss since its imported above
 config.autoAddCss = false
+// Added FontAwesome for icon use in charts
+defaults.font.family = "'Helvetica Neue', 'Helvetica', 'Arial', 'Font Awesome 6 Free', sans-serif"
 
 const MyApp = ({ Component, pageProps }) => {
   return (
