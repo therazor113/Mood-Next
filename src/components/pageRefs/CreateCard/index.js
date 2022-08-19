ec614b4a088007d34986ba093931b7f661db5725
Razor <therazor113@gmail.com>
2022-08-19 16:53:59 -0400

    Changed wording

--- src/components/pageRefs/CreateCard/index.js
+++ src/components/pageRefs/CreateCard/index.js
@@ -8,11 +8,11 @@ const LoginCard = () => {
       <div className={classes.container}>
         <div className={classes.header}>
           <h1>Create Login</h1>
-          <h2>Enter a Name and Password<br/>to create an account.</h2>
+          <h2>Enter a your Name and Password<br/>to create an account.</h2>
         </div>
         <CreateUserForm />
         <div className={classes.footer}>
-          <p>Already A Member?</p>
+          <p>Already created an account?</p>
           <Link href='/'>
             <span className={classes.link}>Sign In</span>
           </Link>
