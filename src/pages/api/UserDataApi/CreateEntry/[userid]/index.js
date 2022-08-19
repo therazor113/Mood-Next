48ea498d78207f8eda00a2ff23bcf23ed449c67c
Razor <therazor113@gmail.com>
2022-08-19 16:59:18 -0400

    Changed method error return

--- src/pages/api/UserDataApi/CreateEntry/[userid]/index.js
+++ src/pages/api/UserDataApi/CreateEntry/[userid]/index.js
@@ -3,33 +3,33 @@ import pool from 'lib/db'
 
 const CreateJournalEntry = async (req, res) => {
   try {
-    if (req.method === 'POST') {
-      const { userid } = req.query
-      const { mood, entry, timeStamp } = req.body
-      const entryListFull = await pool.query(
-        'SELECT * FROM moods WHERE userid = $1 AND date = $2',
-        [userid, timeStamp.date]
-      )
-      if (entryListFull.rows.length >= 8) {
-        res.json('You\'ve finished your entries for today!')
-        return
-      }
-      const entryExists = await pool.query(
-        'SELECT EXISTS (SELECT * FROM moods WHERE userid = $1 AND time = $2 AND date = $3)',
-        [userid, timeStamp.hour, timeStamp.date]
-      )
-      if (entryExists.rows[0].exists) {
-        res.json('Please wait 1 hour for next entry')
-        return
-      }
-      await pool.query(
-        'INSERT INTO moods (mood, journal, date, time, weekday, weekYear, userid) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-        [mood, entry, timeStamp.date, timeStamp.hour, timeStamp.weekDay, timeStamp.weekYear, userid]
-      )
-      res.json(`Journal: ${entry} with mood: ${mood} created`)
-    } else {
-      res.json('only \'POST\' request here')
+    if (req.method !== 'POST') {
+      res.json('only \'POST\' requests here')
+      return
     }
+    const { userid } = req.query
+    const { mood, entry, timeStamp } = req.body
+    const entryListFull = await pool.query(
+      'SELECT * FROM moods WHERE userid = $1 AND date = $2',
+      [userid, timeStamp.date]
+    )
+    if (entryListFull.rows.length >= 8) {
+      res.json('You\'ve finished your entries for today!')
+      return
+    }
+    const entryExists = await pool.query(
+      'SELECT EXISTS (SELECT * FROM moods WHERE userid = $1 AND time = $2 AND date = $3)',
+      [userid, timeStamp.hour, timeStamp.date]
+    )
+    if (entryExists.rows[0].exists) {
+      res.json('Please wait 1 hour for next entry')
+      return
+    }
+    await pool.query(
+      'INSERT INTO moods (mood, journal, date, time, weekday, weekYear, userid) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+      [mood, entry, timeStamp.date, timeStamp.hour, timeStamp.weekDay, timeStamp.weekYear, userid]
+    )
+    res.json(`Journal: ${entry} with mood: ${mood} created`)
   } catch (err) {
     console.error(err.message)
   }
