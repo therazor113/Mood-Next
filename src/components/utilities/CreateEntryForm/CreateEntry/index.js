a598d30c158f6be596764be395a9f6d1027a0eae
Razor <therazor113@gmail.com>
2022-08-20 20:27:33 -0400

    Fix for 24 hr format showing 0 instead of 24

--- src/components/utilities/CreateEntryForm/CreateEntry/index.js
+++ src/components/utilities/CreateEntryForm/CreateEntry/index.js
@@ -4,7 +4,7 @@ const CreateEntry = async (handleFetch, inputEntry, currentMood, userid) => {
     date: date.toLocaleDateString('en-CA'),
     hour: date.getHours(),
     weekDay: date.getDay() === 0 ? 6 : date.getDay() - 1,
-    weekYear: date.getWeek()
+    weekYear: date.getWeek() ? date.getWeek() : 24
   }
   const path = `/UserDataApi/CreateEntry/${userid}`
   const body = { entry: inputEntry, mood: currentMood, timeStamp }
