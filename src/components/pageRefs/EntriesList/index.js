01014233f900c330bac7d26dfc24cd04a635a76a
Razor <therazor113@gmail.com>
2022-08-20 20:22:31 -0400

    Added day mapping and scroll fix

--- src/components/pageRefs/EntriesList/index.js
+++ src/components/pageRefs/EntriesList/index.js
@@ -1,32 +1,44 @@
 import UpdateEntryForm from 'components/utilities/UpdateEntryForm'
+import { useEffect, useRef } from 'react'
 
-const EntriesList = ({ statsArr, cardIndex, updateStats, cardData }) => {
+import classes from './styles.module.scss'
+
+const EntriesList = ({ type, statsArr, updateStats, cardData }) => {
+  const scrollRef = useRef()
+
+  // Scroll to top when entries are opened
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0)
+  }, [])
+
   return (
-    <div>
-      {cardData && cardIndex !== null &&
-        statsArr.map(userStat => {
-          if (userStat.mood === cardData) {
-            return (
-          <ul style={{ listStyleType: 'none' }}
-          key={userStat.id}
-          >
-            <li>Mood: {userStat.mood}</li>
-            <li>Journal Entry: {userStat.journal}</li>
-            <UpdateEntryForm
-              updateStats={updateStats}
-              entryId={userStat.id}
-            />
-          </ul>
-            )
-          } return (null)
-        })
+    <div className={classes.container} ref={scrollRef}>
+      {type === 'day' && statsArr &&
+        <ul key={statsArr.id}>
+          <li>{statsArr.date}</li>
+          <li>Mood: {statsArr.mood}</li>
+          <li>Journal Entry: {statsArr.journal}</li>
+          <UpdateEntryForm
+            updateStats={updateStats}
+            entryId={statsArr.id}
+          />
+        </ul>
       }
 
-      {statsArr[cardIndex]?.entries && cardIndex !== null &&
-        statsArr[cardIndex]?.entries.map((userStats) => (
-          <ul style={{ listStyleType: 'none' }}
-          key={userStats.id}
-          >
+      {type === 'week' && statsArr &&
+        statsArr.map((userStats) => (
+          <ul key={userStats.id}>
+            <li>Hour: {
+             userStats.time < 12
+               ? `${userStats.time} AM`
+               : userStats.time === 12
+                 ? `${userStats.time} PM`
+                 : userStats.time === 24
+                   ? `${userStats.time - 12} AM`
+                   : `${userStats.time - 12} PM`
+                // Sometimes it just be like that
+                }
+            </li>
             <li>Mood: {userStats.mood}</li>
             <li>Journal Entry: {userStats.journal}</li>
             <UpdateEntryForm
@@ -35,6 +47,23 @@ const EntriesList = ({ statsArr, cardIndex, updateStats, cardData }) => {
             />
           </ul>
         ))
+      }
+
+      {type === 'month' && statsArr &&
+        statsArr.map(userStat => {
+          if (userStat.mood === cardData) {
+            return (
+          <ul key={userStat.id}>
+            <li>Mood: {userStat.mood}</li>
+            <li>Journal Entry: {userStat.journal}</li>
+            <UpdateEntryForm
+              updateStats={updateStats}
+              entryId={userStat.id}
+            />
+          </ul>
+            )
+          } else return undefined
+        })
       }
   </div>
   )
