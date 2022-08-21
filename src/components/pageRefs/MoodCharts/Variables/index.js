af76e6450b6e1e6d77787b2afe79bf2e309c6278
Razor <therazor113@gmail.com>
2022-08-20 20:23:49 -0400

    Added chart variables

---
+++ src/components/pageRefs/MoodCharts/Variables/index.js
@@ -0,0 +1,47 @@
+export const colors = [
+  'rgba(255, 0, 0, 0.7)',
+  'rgba(255, 68, 0, 0.7)',
+  'rgba(255, 119, 0, 0.7)',
+  'rgba(255, 204, 0, 0.7)',
+  'rgba(255, 255, 0, 0.7)',
+  'rgba(200, 225, 0, 0.7)',
+  'rgba(120, 200, 0, 0.7)',
+  'rgba(55, 225, 0, 0.7)',
+  'rgba(0, 200, 0, 0.9)'
+]
+
+export const solidColors = [
+  'rgba(255, 0, 0)',
+  'rgba(255, 68, 0)',
+  'rgba(255, 119, 0)',
+  'rgba(255, 204, 0)',
+  'rgba(255, 255, 0)',
+  'rgba(200, 225, 0)',
+  'rgba(120, 200, 0)',
+  'rgba(55, 225, 0)',
+  'rgba(0, 200, 0)'
+]
+
+export const icons = [
+  '\uf567',
+  '\uf5b3',
+  '\uf119',
+  '\uf57a',
+  '\uf11a',
+  '\uf118',
+  '\uf5b8',
+  '\uf582',
+  '\uf59a'
+]
+
+export const createGradient = (ctx, area) => {
+  const colorStart = 'rgb(255, 0, 0)'
+  const colorMid = 'rgba(200, 225, 0)'
+  const colorEnd = 'rgb(0, 200, 0)'
+  const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top)
+  gradient.addColorStop(0, colorStart)
+  gradient.addColorStop(0.5, colorMid)
+  gradient.addColorStop(1, colorEnd)
+
+  return gradient
+}
