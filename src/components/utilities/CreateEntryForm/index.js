b1868a1ad0b205e4d4ef58eb369b6133356f28ce
Razor <therazor113@gmail.com>
2022-08-21 15:42:51 -0400

    Added animation and styling to createEntry card

--- src/components/utilities/CreateEntryForm/index.js
+++ src/components/utilities/CreateEntryForm/index.js
@@ -1,30 +1,28 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import MoodIcons from 'components/pageRefs/UserPageRef/MoodIcons'
 import CreateEntry from './CreateEntry'
 import useAPI from 'hooks/useAPI'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faAngleLeft, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import classes from './styles.module.scss'
 
 const CreateEntryForm = ({ updateStats, userid }) => {
   const [currentMood, setCurrentMood] = useState(null)
   const [inputEntry, setInputEntry] = useState('')
+  const [startEntry, setStartEntry] = useState(false)
   const [loading, setLoading] = useState(false)
-  const [message, setMessage] = useState('')
-  const [show, setShow] = useState(false)
   const [handleFetch] = useAPI()
-
-  useEffect(() => currentMood ? setShow(true) : setShow(false), [currentMood])
 
   const handleEnter = async () => {
     if (!inputEntry || !currentMood) return
     setLoading(true)
     const data = await CreateEntry(handleFetch, inputEntry, currentMood, userid)
-    if (!data) {
-      setMessage('An Error has occurred')
-      setTimeout(() => setMessage(''), 1500)
+    if (data) {
+      setInputEntry('')
+      updateStats()
     }
-    setLoading(false)
-    updateStats()
+    setTimeout(() => setLoading(false), 1000)
   }
 
   const handleInputEntry = (e) => {
@@ -34,16 +32,40 @@ const CreateEntryForm = ({ updateStats, userid }) => {
 
   return (
     <main className={classes.container}>
-      {!show &&
-      <div className={classes.selectMood}>
-        <h2>How are you feeling?</h2>
-        <MoodIcons setCurrentMood={setCurrentMood} />
+      <div
+        className={classes.startEntry}
+        onClick={() => setStartEntry(true)}
+        style={startEntry ? { transform: 'translate(-190%, 10%)', opacity: 0 } : undefined}
+      >
+      <span>
+        <FontAwesomeIcon icon={faPlus} />
+      </span>
+        <h2>Add a new entry?</h2>
       </div>
-      }
 
-      {show &&
-      <div className={classes.journalEntry} >
-        <h2>Journal</h2>
+      <div
+        className={classes.selectMood}
+        style={startEntry
+          ? !currentMood
+              ? { transform: 'translateX(0%)', opacity: 1 }
+              : { transform: 'translateX(-140%)', opacity: 0 }
+          : { transform: 'translateX(140%)', opacity: 0 }}
+      >
+        <h2>How are you feeling?</h2>
+        <MoodIcons
+          setCurrentMood={setCurrentMood}
+        />
+        <FontAwesomeIcon
+          icon={faAngleLeft}
+          className={classes.arrow}
+          onClick={() => setStartEntry(false)}
+        />
+      </div>
+
+      <div
+        className={classes.journalEntry}
+        style={currentMood && { transform: 'translateX(0)', opacity: 1 }}
+      >
         <textarea
           name='message'
           aria-label='Journal'
@@ -51,18 +73,24 @@ const CreateEntryForm = ({ updateStats, userid }) => {
           value={inputEntry}
           onChange={handleInputEntry}
         />
-        <input
-          type='button'
-          value={loading ? 'Sending...' : 'Send'}
-          onClick={handleEnter}
-        />
-        <p>{message}</p>
-        <button onClick={() => {
-          setCurrentMood(null)
-          setInputEntry('')
-        }}>Cancel</button>
+        <div>
+          <FontAwesomeIcon
+            icon={faSpinner}
+            style={loading && { opacity: 1 }}
+            spin
+          />
+          <input
+            type='button'
+            value='Send'
+            onClick={handleEnter}
+          />
+        </div>
+          <FontAwesomeIcon
+            icon={faAngleLeft}
+            className={classes.arrow}
+            onClick={() => setCurrentMood(null)}
+          />
       </div>
-      }
     </main>
   )
 }
