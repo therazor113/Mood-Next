50ba974cad4f7b54cd0dda5414ca30607db48a0a
Razor <therazor113@gmail.com>
2022-08-20 20:25:01 -0400

    Fix to show empty weekdays with no entries

--- src/components/pageRefs/UserPageData/WeekStats/index.js
+++ src/components/pageRefs/UserPageData/WeekStats/index.js
@@ -1,7 +1,18 @@
 const WeekStats = async (handleFetch, user, setStats, setMoods) => {
-  const data = await handleFetch(`/UserDataApi/GetEntries/${user.userid}/GetWeek/${new Date().getWeek()}`, 'GET')
+  const data = await handleFetch(
+    `/UserDataApi/GetEntries/${user.userid}/GetWeek/${new Date().getWeek()}`,
+    'GET'
+  )
   setStats(prev => ({ ...prev, week: data[0] }))
-  setMoods(prev => ({ ...prev, week: data[1] }))
+  setMoods(prev => {
+    prev.week = [{}, {}, {}, {}, {}, {}, {}]
+    for (let i = 0; i < prev.week.length; i++) {
+      if (data[1]?.[i]) {
+        prev.week[data[1][i].weekday] = data[1][i].avg
+      }
+    }
+    return { ...prev, week: prev.week }
+  })
 }
 
 export default WeekStats
