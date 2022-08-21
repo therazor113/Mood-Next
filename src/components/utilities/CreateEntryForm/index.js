2c9a8c9b31b385535f4054b17304ec718c92bb46
Razor <therazor113@gmail.com>
2022-08-20 20:26:15 -0400

    Working on create mood card

--- src/components/utilities/CreateEntryForm/index.js
+++ src/components/utilities/CreateEntryForm/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import MoodIcons from 'components/pageRefs/UserPageRef/MoodIcons'
 import CreateEntry from './CreateEntry'
 import useAPI from 'hooks/useAPI'
@@ -8,41 +8,61 @@ import classes from './styles.module.scss'
 const CreateEntryForm = ({ updateStats, userid }) => {
   const [currentMood, setCurrentMood] = useState(null)
   const [inputEntry, setInputEntry] = useState('')
+  const [loading, setLoading] = useState(false)
   const [message, setMessage] = useState('')
+  const [show, setShow] = useState(false)
   const [handleFetch] = useAPI()
+
+  useEffect(() => currentMood ? setShow(true) : setShow(false), [currentMood])
 
   const handleEnter = async () => {
     if (!inputEntry || !currentMood) return
+    setLoading(true)
     const data = await CreateEntry(handleFetch, inputEntry, currentMood, userid)
-    setMessage(data)
-    setTimeout(() => setMessage(''), 1500)
+    if (!data) {
+      setMessage('An Error has occurred')
+      setTimeout(() => setMessage(''), 1500)
+    }
+    setLoading(false)
     updateStats()
   }
 
   const handleInputEntry = (e) => {
-    if (e.target.value.length > 255) return
+    if (e.target.value.length > 725) return
     setInputEntry(e.target.value)
-  }
-
-  const handleKeyUp = (e) => {
-    if (e.key === 'Enter') {
-      handleEnter()
-    }
   }
 
   return (
     <main className={classes.container}>
-      <p className={classes.message}>{message}</p>
-      {currentMood}
-      <button onClick={handleEnter}>Create</button>
-      <MoodIcons setCurrentMood={setCurrentMood}/>
-      <input
-        placeholder='Journal Entry'
-        className={classes.entryInput}
-        value={inputEntry}
-        onChange={handleInputEntry}
-        onKeyUp={handleKeyUp}
-      />
+      {!show &&
+      <div className={classes.selectMood}>
+        <h2>How are you feeling?</h2>
+        <MoodIcons setCurrentMood={setCurrentMood} />
+      </div>
+      }
+
+      {show &&
+      <div className={classes.journalEntry} >
+        <h2>Journal</h2>
+        <textarea
+          name='message'
+          aria-label='Journal'
+          placeholder='Whats on your mind?'
+          value={inputEntry}
+          onChange={handleInputEntry}
+        />
+        <input
+          type='button'
+          value={loading ? 'Sending...' : 'Send'}
+          onClick={handleEnter}
+        />
+        <p>{message}</p>
+        <button onClick={() => {
+          setCurrentMood(null)
+          setInputEntry('')
+        }}>Cancel</button>
+      </div>
+      }
     </main>
   )
 }
