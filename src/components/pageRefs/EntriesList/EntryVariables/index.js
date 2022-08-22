ff1f4b9a83db80204cbb33b6074161765fd7fc77
Razor <therazor113@gmail.com>
2022-08-22 15:21:45 -0400

    Created variables folder for userStats

---
+++ src/components/pageRefs/EntriesList/EntryVariables/index.js
@@ -0,0 +1,37 @@
+import { faLaughBeam, faGrinBeam, faSmileBeam, faSmile, faFaceMeh, faFaceFrownOpen, faFaceFrown, faFaceSadCry, faFaceDizzy } from '@fortawesome/free-solid-svg-icons'
+
+export const entryIcons = [
+  faFaceDizzy,
+  faFaceSadCry,
+  faFaceFrown,
+  faFaceFrownOpen,
+  faFaceMeh,
+  faSmile,
+  faSmileBeam,
+  faGrinBeam,
+  faLaughBeam
+]
+
+export const entryColors = [
+  'rgb(255, 0, 0)',
+  'rgb(255, 68, 0)',
+  'rgb(255, 119, 0)',
+  'rgb(255, 204, 0)',
+  'rgb(255, 255, 0)',
+  'rgb(200, 225, 0)',
+  'rgb(120, 200, 0)',
+  'rgb(55, 225, 0)',
+  'rgb(0, 200, 0)'
+]
+
+export const entryBackgrounds = [
+  'rgba(255, 0, 0, 0.1)',
+  'rgba(255, 68, 0, 0.1)',
+  'rgba(255, 119, 0, 0.1)',
+  'rgba(255, 204, 0, 0.1)',
+  'rgba(255, 255, 0, 0.1)',
+  'rgba(200, 225, 0, 0.1)',
+  'rgba(120, 200, 0, 0.1)',
+  'rgba(55, 225, 0, 0.1)',
+  'rgba(0, 200, 0, 0.1)'
+]
