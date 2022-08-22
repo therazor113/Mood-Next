696bb9ff0ad37e696fa83cae925716cea9f33d89
Razor <therazor113@gmail.com>
2022-08-22 15:29:08 -0400

    Made textArea exportable

---
+++ src/components/utilities/FormTextField/index.js
@@ -0,0 +1,16 @@
+const FormTextField = ({ handleChange, inputEntry, textLength, classes }) => {
+  return (
+  <div className={classes.textAreaContainer}>
+    <textarea
+      name='message'
+      aria-label='Journal'
+      placeholder='Whats on your mind?'
+      value={inputEntry}
+      onChange={handleChange}
+    />
+    <p className={classes.textLength}>{textLength}</p>
+  </div>
+  )
+}
+
+export default FormTextField
