939a00a9d032b0138861e583ac6cccc7a23d0106
Razor <therazor113@gmail.com>
2022-08-23 21:08:37 -0400

    Linting

--- src/pages/api/UserDataApi/UpdateEntry/[id]/index.js
+++ src/pages/api/UserDataApi/UpdateEntry/[id]/index.js
@@ -2,16 +2,16 @@ import pool from 'lib/db'
 
 const UpdateEntryByid = async (req, res) => {
   try {
-    if (req.method === 'PUT') {
-      const { id } = req.query
-      const { entry, mood } = req.body
-      await pool.query('UPDATE moods SET mood = $1, journal = $2 WHERE id = $3',
-        [mood, entry, id]
-      )
-      res.json(`Journal Updated: entry ${entry} mood: ${mood}`)
-    } else {
+    if (req.method !== 'PUT') {
       res.json('only \'PUT\' requests here')
+      return
     }
+    const { id } = req.query
+    const { entry, mood } = req.body
+    await pool.query('UPDATE moods SET mood = $1, journal = $2 WHERE id = $3',
+      [mood, entry, id]
+    )
+    res.json(`Journal Updated: entry ${entry} mood: ${mood}`)
   } catch (err) {
     console.error(err.message)
   }
