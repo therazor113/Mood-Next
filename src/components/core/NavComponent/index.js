81b3358cf1e4b6d0b2a293296126895a08a20d35
Razor <therazor113@gmail.com>
2022-08-24 00:04:40 -0400

    Fix for logged in navbar

--- src/components/core/NavComponent/index.js
+++ src/components/core/NavComponent/index.js
@@ -1,22 +1,25 @@
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 import classes from './styles.module.scss'
 
 const NavComponent = ({ log }) => {
+  const router = useRouter()
   const handleClick = async () => {
     await fetch('/api/CookieApi')
+    router.push('/')
   }
 
   return (
     <div className={classes.container}>
       <h2>Mood-<span className={classes.next}>Next</span></h2>
       <div>
+        {log === 'in' &&
         <Link href='/createLogin'>
           <h2 className={classes.link}>Register</h2>
           </Link>
-        <Link href='/'>
+          }
           <h2 className={classes.link} onClick={handleClick}>Log {log}</h2>
-        </Link>
       </div>
     </div>
   )
