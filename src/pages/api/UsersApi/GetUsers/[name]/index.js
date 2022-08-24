a14c4728cc38c0aa73275627561656f2e6eb4cec
Razor <therazor113@gmail.com>
2022-08-23 21:09:01 -0400

    Fixed to only grab userid & name

--- src/pages/api/UsersApi/GetUsers/[name]/index.js
+++ src/pages/api/UsersApi/GetUsers/[name]/index.js
@@ -3,7 +3,7 @@ import pool from 'lib/db'
 const getUserName = async (req, res) => {
   try {
     const { name } = req.query
-    const user = await pool.query('SELECT * FROM users WHERE name = $1', [name])
+    const user = await pool.query('SELECT userid, name FROM users WHERE name = $1', [name])
     res.json(user.rows[0])
   } catch (err) {
     console.error(err.message)
