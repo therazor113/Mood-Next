9680aa29bfa2bbf27c7ebfb3f940a431193868f2
Razor <therazor113@gmail.com>
2022-08-23 21:06:05 -0400

    Linting

--- src/components/core/NavComponent/index.js
+++ src/components/core/NavComponent/index.js
@@ -7,10 +7,10 @@ const NavComponent = ({ log }) => {
     <div className={classes.container}>
       <h2>Mood-<span className={classes.next}>Next</span></h2>
       <div>
-        <Link href={'/createLogin'}>
+        <Link href='/createLogin'>
           <h2 className={classes.link}>Register</h2>
           </Link>
-        <Link href={'/'}>
+        <Link href='/'>
           <h2 className={classes.link}>Log {log}</h2>
         </Link>
       </div>
