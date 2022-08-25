05a13daaf70aef07203c797978ca56e9da3d4529
Razor <therazor113@gmail.com>
2022-08-25 19:37:11 -0400

    Added date to week stats

--- src/pages/api/UserDataApi/GetEntries/[userid]/GetWeek/[week]/index.js
+++ src/pages/api/UserDataApi/GetEntries/[userid]/GetWeek/[week]/index.js
@@ -4,7 +4,7 @@ const getAllMoodsById = async (req, res) => {
   try {
     const { userid, week } = req.query
     const moods = await pool.query(
-      'SELECT weekday, json_agg(json_build_object(\'id\', id, \'mood\', mood, \'time\', time, \'journal\', journal) ORDER BY time) AS entries FROM moods WHERE userid = $1 AND weekyear = $2 GROUP BY weekday ORDER BY weekday',
+      'SELECT weekday, json_agg(json_build_object(\'id\', id, \'mood\', mood, \'time\', time, \'date\', date, \'journal\', journal) ORDER BY time) AS entries FROM moods WHERE userid = $1 AND weekyear = $2 GROUP BY weekday ORDER BY weekday',
       [userid, week]
     )
     const avgMoods = await pool.query(
